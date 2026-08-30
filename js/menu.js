690f2a51b9c101b1d3f4d93d6a1af57e3a7bbec6
nicogutsche9-boop <nicogutsche9@icloud.com>
2026-08-30 11:09:16 +0200

    Delete js directory

--- js/menu.js
+++
@@ -1,5 +0,0 @@
-export function showScreen(id) {
-  document.querySelectorAll(".screen").forEach(screen => screen.classList.remove("active"));
-  const target = document.getElementById(id);
-  if (target) target.classList.add("active");
-}
